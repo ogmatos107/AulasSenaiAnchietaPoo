02b51957377e6461ebf18d587a221119290781c8
ogmatos <pedraao107@gmail.com>
2024-12-09 14:47:05 -0300

    hvkvvlvl

--- src/main/java/Aula02_colecoes/exercicios/exercicio01/Main.java
+++ src/main/java/Aula02_colecoes/exercicios/exercicio01/Main.java
@@ -1,38 +1,62 @@
 package Aula02_colecoes.exercicios.exercicio01;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Tarefa> listaTarefas = new ArrayList<>();
 
-        String menu = "-----Gerenciador de Tarefas-----\n" +
-                "1 - Adicionar tarefa\n" +
-                "2 - Listar tarefas\n" +
-                "3 - Marcar tarefa como concluída\n" +
-                "4 - Remover tarefa\n" +
-                "5 - Sair";
-        System.out.println("escolha um opção: ");
-        int opcao = scanner.nextInt();
+        Tarefa tarefa1 = new Tarefa("", true);
+        Tarefa tarefa2 = new Tarefa("", true);
 
-        switch (opcao) {
-            case 1:
-                scanner.nextLine();
-                System.out.println("digite o nome da tarefa  ");
-                break;
-            case 2:
-                System.out.println("");
-                break;
-            case 3:
-                System.out.println("qu");
-                break;
-            case 4:
-                System.out.println("");
-                break;
-            case 5:
-                System.out.println("");
-                break;
+        System.out.println(tarefa1);
+        System.out.println(tarefa2);
 
+        System.out.println(tarefa1.equals(tarefa2));
+        String menu = """
+                    Escolha:
+                        1 - adicionar tarefa
+                        2 - exibir tarefa
+                        3 - atualizar tarefa
+                        4 - deletar tarefa
+                """;
+        int opcao;
+        do {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome da tarefa");
+                    String nomeTarefa = scanner.nextLine();
+                    listaTarefas.add(new Tarefa(nomeTarefa, false));
+                    System.out.println("Tarefa cadastrada com sucesso!!");
+                    break;
+                case 2:
+                    exibirTarefas();
+                    break;
+                case 3:
+                    exibirTarefas();
+                    System.out.println("Escolha uma tarefa pelo id para marcar como concluida!");
+                    listaTarefas.get(scanner.nextInt() - 1).status = true;
+                    System.out.println("Tarefa atualizada com sucesso!!");
+                    break;
+                case 4:
+                    exibirTarefas();
+                    System.out.println("Escolha uma tarefa pelo id para ser removida");
+                    listaTarefas.remove(scanner.nextInt() - 1);
+                    System.out.println("Tarefa removida com sucesso!!");
+                    break;
+                case 5:
+                    System.out.println("Fim Programa");
+                    scanner.close();
+                    break;
+            }
+        } while (opcao != 5);
+    }
+
+    public static void exibirTarefas() {
         }
     }
-}
--- src/main/java/Aula02_colecoes/exercicios/exercicio01/Tarefa.java
+++ src/main/java/Aula02_colecoes/exercicios/exercicio01/Tarefa.java
@@ -1,8 +1,27 @@
 package Aula02_colecoes.exercicios.exercicio01;
 
-public class Tarefa {
-    String nomeTarefa;
-    Boolean statusTarefa;
+import java.util.Objects;
 
+public class Tarefa {
+    String nome;
+    boolean status;
+
+    public Tarefa(String nome,boolean status) {
+        this.status = status;
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefas = (Tarefa) o;
+        return status == tarefas.status && Objects.equals(nome, tarefas.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, status);
+    }
 }
 
